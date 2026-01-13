2406cd27428a9303771a75028f65dab908e849b4
Hanush <112359676+woker21@users.noreply.github.com>
2026-01-13 17:11:25 +0100

    problema al hacer login resuelto ( closes #2 )

---
+++ app/(shell)/perfil/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            <h1>Página de perfil</h1>
+            <p>Contenido del perfil va aquí.</p>
+        </div>
+    )
+}
+
+export default page
